5a205efd00ad1135925a07a6e0286e642732d12d
JZims <zimmerman.j@hotmail.com>
2021-08-10 22:17:20 -0400

    JS answered

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,15 @@
 function findShortestString(arr) {
-  // type your code here
+  //Declare a variable to hold shortest string
+  
+  let shortest = arr[0]
+  //loop through array to compare each string to the string held in shortest variable
+  for(var i = 0; i < arr.length; i++){
+    //During each iteration, compare the string's length to the length of the string held in the variable
+    if(arr[i].length < shortest.length) shortest = arr[i]
+    //If the variable is shorter, replace the shortest string
+  }
+  //Return the shortest string
+  return shortest
 }
 
 if (require.main === module) {
@@ -23,4 +33,11 @@ if (require.main === module) {
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+
+
+
+
+
+
+
 // And a written explanation of your solution
